aa4a0cd1b4a036cc4f9feb4c56a62f805ea156e0
Kolja Pohl <kolja.pohl@pohlcon.com>
2020-12-14 11:35:53 +0100

    implemented strike and delete functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,26 @@
-    let list = $('#list');
+let list = $('#list');
 
-    function newItem() {
-        let input = $('#input');
-        let value = input.val();
-        let li = $('<li></li>');
-
-        li.append(value);
+function newItem() {
+    //adding a new item to the list of items
+    let input = $('#input');
+    let value = input.val(); //assigns user input to the variable value
+    let li = $('<li></li>'); //create new li-item
+    //if the user input is not empty, adds li-item to the end of the list of items
+    if (value == '') {
+        alert('Empty tasks are prohibited');
+    } else {
+        li.append(value); //assign user input to the li-item
         list.append(li);
-
-        //want to clear #input after adding the li-item to List
-
+        input.val(''); //clears the input after adding an item
     }
+    li.on('click', function() {
+        li.toggleClass('strike'); //toggles class .strike on clicked li-element
+    });
+    let delButton = $('<deleteButton></deleteButton>');
+    delButton.addClass('deleteButton');
+    delButton.append(document.createTextNode('X'));
+    li.append(delButton);
+    delButton.on('click', function() {
+        li.addClass('delete');
+    });
+}
